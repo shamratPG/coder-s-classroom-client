73c7bd148825f3a9539c69a6e10f0a5912841961
shamratPG <shamratpg@gmail.com>
2022-10-25 15:53:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [course, setCourse] = useState([]);
+  useEffect(() => {
+    fetch('https://coders-classroom-server.vercel.app/blogs')
+      .then(data => data.json())
+      .then(result => {
+        console.log(result);
+        setCourse(result);
+      })
+      .catch(error => console.error(error))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {course.length}
+
     </div>
   );
 }
